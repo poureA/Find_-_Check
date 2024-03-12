9138c2a616e60aef09dd1e1f54f21009a6afd983
poureA <101800359+poureA@users.noreply.github.com>
2024-03-13 01:10:15 +0330

    Update Find_&_Check.py

--- Find_&_Check.py
+++ Find_&_Check.py
@@ -4,7 +4,6 @@ def Find(path)->None:
     translator = Translator()
     with open(path,'r') as file:
         result = [note for note in file.readlines() if translator.detect(note).lang=='en']
-        new_path = '\\'.join([i for i in path.split('\\')[:-1]])
         with open('export.txt','w') as dest_file:
             dest_file.writelines(result)
 Find("inputs.txt")
